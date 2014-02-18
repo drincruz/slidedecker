7b7b3feff72401fdd173b8fd2ebc781d50b08a67
Adrian Cruz <drincruz@gmail.com>
2014-02-18 01:09:16 -0500

    working on full screen templates

--- slidedecker.py
+++ slidedecker.py
@@ -6,14 +6,17 @@ SlideDecker
 Presentations for web browsers
 """
 
-from flask import Flask
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello you"
+# TODO get rid of this for "production"-ready
+app.debug = True
+
+@app.route('/')
+def slides():
+    return render_template("slide.html")
 
 
 if "__main__" == __name__:
