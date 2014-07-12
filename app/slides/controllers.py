9f0b3a2f6eece20b6008a7d119910551c954ec12
Adrian Cruz <drincruz@gmail.com>
2014-07-12 02:36:28 -0400

    WIP started moving over code to use the reorganized structure

---
+++ app/slides/controllers.py
@@ -0,0 +1,10 @@
+from app import db
+from app.slides.models import Slide
+from flask import (Blueprint, Flask, flash, redirect, render_template,
+        url_for)
+
+slides = Blueprint('slides', __name__, url_prefix='/slides')
+
+@slides.route('/')
+def slides():
+    return render_template("slide.html")
