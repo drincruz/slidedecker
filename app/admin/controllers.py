016b4981b1bd17a9ad6e0494d15a8cb84cc72bfd
Adrian Cruz <drincruz@gmail.com>
2014-07-17 00:22:27 -0400

    added request from flask so i can get the POST data

--- app/admin/controllers.py
+++ app/admin/controllers.py
@@ -5,7 +5,7 @@ Admin interface controllers
 
 from app import db
 from app.slides.models import Slide
-from flask import Blueprint, flash, redirect, render_template, url_for
+from flask import Blueprint, flash, redirect, request, render_template, url_for
 
 admin = Blueprint('admin', __name__, url_prefix='/admin')
 
@@ -16,5 +16,9 @@ def admin_home():
 @admin.route('/add', methods=['POST'])
 def add_slide():
     # TODO store slide data
+    title = request.form['title']
+    bg_image = request.form['bg-image']
+    bg_color = request.form['bg-color']
+    text = request.form['text']
     flash('New slide was successfully posted')
     return redirect(url_for('admin.admin_home'))
